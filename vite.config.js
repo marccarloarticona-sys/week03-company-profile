f7967e8934e3c617a8246c8d00bf0dd34d3eef27
Marc Carlo Articona <marccarloarticona1@gmail.com>
2026-08-14 23:03:05 +0800

    build: fix Vite asset configuration

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/navbar.css',
+                'resources/css/footer.css',
+                'resources/css/home.css',
+                'resources/css/about.css',
+                'resources/css/services.css',
+                'resources/css/contact.css',
+
+                'resources/js/navbar.js',
+                'resources/js/footer.js',
+                'resources/js/home.js',
+                'resources/js/about.js',
+                'resources/js/services.js',
+                'resources/js/contact.js'
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
